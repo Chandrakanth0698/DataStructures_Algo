cea35071f8df5e8567a6e360af24a2d6fe9ab072
Chandrakanth0698 <chandrakanth0698@gmail.com>
2023-12-15 12:09:31 -0500

    Initial Commit

---
+++ O(n).py
@@ -0,0 +1,11 @@
+"""
+O(n) - the number of operations performed will be linear with
+number of input values. The graph of O(n) is always linear.
+"""
+n = 10
+for i in range(n):
+    print()
+"""
+the time complexity of the above code is O(n) since the print operation runs 
+with respect to number of value of n.
+"""
